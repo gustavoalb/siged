a636dfd4ea7133b277b519b1cd40308da95421b5
Gustavo Albuquerque <alucard.ap@gmail.com>
2012-06-29 12:39:29 -0300

    Show Escolas

--- app/controllers/disciplina_contratacoes_controller.rb
+++ app/controllers/disciplina_contratacoes_controller.rb
@@ -4,8 +4,8 @@ class DisciplinaContratacoesController < ApplicationController
   # GET /descricao_cargos.xml
   before_filter :dados_essenciais
   def index
-    #@search = DisciplinaContratacao.scoped_search(params[:search])
-    @disciplina_contratacoes = DisciplinaContratacao.order(:codigo,:nome).paginate :page => params[:page], :order => 'created_at DESC', :per_page => 10
+    @search = DisciplinaContratacao.scoped_search(params[:search])
+    @disciplina_contratacoes = @search.order(:codigo,:nome).paginate :page => params[:page], :order => 'created_at DESC', :per_page => 10
 
     respond_to do |format|
       format.html # index.html.erb
--- app/models/disciplina_contratacao.rb
+++ app/models/disciplina_contratacao.rb
@@ -1,9 +1,12 @@
 class DisciplinaContratacao < ActiveRecord::Base
 	#default_scope where('entidade_id in (?)',User.usuario_atual.entidade_ids)
+
 	set_table_name :descricao_cargos
-  belongs_to :tipo
-  belongs_to :nivel_cargo
-  belongs_to :entidade
-  has_many :funcionarios
-  has_and_belongs_to_many :disciplinas,:class_name=>"Disciplina",:join_table => "disciplinas_habilitacoes"
+	include ScopedSearch::Model
+	scope :busca, lambda { |q| where("codigo ilike ? or nome ilike ?" ,"%#{q}%","%#{q}%") }
+	belongs_to :tipo
+	belongs_to :nivel_cargo
+	belongs_to :entidade
+	has_many :funcionarios
+	has_and_belongs_to_many :disciplinas,:class_name=>"Disciplina",:join_table => "disciplinas_habilitacoes"
 end
