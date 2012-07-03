183c2abcb5c55e3ff733d3afe694fea39d7278aa
Gustavo Albuquerque <alucard.ap@gmail.com>
2012-07-03 11:26:33 -0300

    1=1?

--- app/models/disciplina.rb
+++ app/models/disciplina.rb
@@ -20,7 +20,7 @@ class Disciplina < ActiveRecord::Base
           else
             fator_sobrando = curriculo.horas_semanal
           end
-          if 1=1#fator_sobrando>0
+          if 1==1 #fator_sobrando>0
             return true
           else
              return false
