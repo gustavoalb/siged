d3bb76c85bf37c1ffb23b292400fb47f2de450d9
Gustavo Albuquerque <richie-guga@hotmail.com>
2012-09-04 11:14:14 -0300

    Feriados

--- config/initializers/feriados.rb
+++ config/initializers/feriados.rb
@@ -1,9 +1,12 @@
+Date::FERIADOS = []
+Date::FERIADOS_METODOS = []
 feriados, metodos = FeriadoParser.parser("#{Rails.root}/config/feriados")
- if File.directory?("#{Rails.root}/config/feriados")
-   f, m = FeriadoParser.parser("#{Rails.root}/config/feriados")
-   feriados += f
-   metodos += m
- end
+if File.directory?("#{Rails.root}/config/feriados")
+	f, m = FeriadoParser.parser("#{Rails.root}/config/feriados")
+	feriados += f
+	metodos += m
+end
 
- feriados.each { |f| Date::FERIADOS << f }
- metodos.each { |m| Date::FERIADOS_METODOS << m }
+feriados.each { |f| Date::FERIADOS << f }
+metodos.each { |m| Date::FERIADOS_METODOS << m }
+
