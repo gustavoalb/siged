070731a079b46710dfe47aa40721fe6a2d78007b
Jeová Guilherme de Carvalho Filho <jguilhermeap@gmail.com>
2015-02-02 15:08:49 -0300

    Correção de Horas

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,8 @@ Siged20::Application.configure do
   config.action_controller.perform_caching = true
 
   # Specifies the header that your server uses for sending files
-  config.action_dispatch.x_sendfile_header = "X-Sendfile"
+  #config.action_dispatch.x_sendfile_header = "X-Sendfile"
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
   #SHARED = Pathname.new("/var/www/siged/shared")
   # For nginx:
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
