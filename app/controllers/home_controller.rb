1f40f5bc80bc606dc6bf2fbd87a8695c244942a9
amapaonrails <rpablo.mv@gmail.com>
2015-08-19 11:33:12 -0300

    mudanças ruan

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,14 @@
 # -*- encoding : utf-8 -*-
 class HomeController < ApplicationController
-	#load_and_authorize_resource
+  #load_and_authorize_resource
 
-	def index
-		@disciplinas = Rails.cache.fetch('disciplinas', :expires_in => 24.hours) { DisciplinaContratacao.find(:all,:joins=>:funcionarios,:order=>['nome asc']).uniq }
-		#@disciplinas = DisciplinaContratacao.find(:all,:joins=>:funcionarios,:order=>['nome asc']).uniq
-		@professores = Funcionario.find_all_by_cargo_id(Cargo.find_by_nome("Professor")).uniq
-		@indefinidos = Funcionario.find(:all,:conditions=>["cargo_id = ? and disciplina_contratacao_id is null",Cargo.find_by_nome("Professor")]).uniq
-		@i_lotados = Funcionario.find(:all,:joins=>:lotacoes_atuais,:conditions=>["cargo_id = ? and disciplina_contratacao_id is null",Cargo.find_by_nome("Professor")]).uniq
-		@i_especificados = Funcionario.find(:all,:joins=>:especificacoes,:conditions=>["cargo_id = ? and disciplina_contratacao_id is null",Cargo.find_by_nome("Professor")]).uniq
-		@noticias = Mensagem.noticias.order(:created_at)
-	end
+  def index
+    @disciplinas = Rails.cache.fetch('disciplinas', :expires_in => 24.hours) { DisciplinaContratacao.find(:all,:joins=>:funcionarios,:order=>['nome asc']).uniq }
+    #@disciplinas = DisciplinaContratacao.find(:all,:joins=>:funcionarios,:order=>['nome asc']).uniq
+    @professores = Funcionario.find_all_by_cargo_id(Cargo.find_by_nome("PROFESSOR")).uniq
+    @indefinidos = Funcionario.find(:all,:conditions=>["cargo_id = ? and disciplina_contratacao_id is null",Cargo.find_by_nome("PROFESSOR")]).uniq
+    @i_lotados = Funcionario.find(:all,:joins=>:lotacoes_atuais,:conditions=>["cargo_id = ? and disciplina_contratacao_id is null",Cargo.find_by_nome("PROFESSOR")]).uniq
+    @i_especificados = Funcionario.find(:all,:joins=>:especificacoes,:conditions=>["cargo_id = ? and disciplina_contratacao_id is null",Cargo.find_by_nome("PROFESSOR")]).uniq
+    @noticias = Mensagem.noticias.order(:created_at)
+  end
 end
-
